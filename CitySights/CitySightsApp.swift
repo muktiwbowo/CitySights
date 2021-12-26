92cdbe44ed4ceb49772f9fd0810a0ca4982f18bb
Mukti Wibowo <muktiwbowo@gmail.com>
2021-12-26 14:47:41 +0700

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Mukti Wibowo on 26/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
